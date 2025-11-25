220d5f7190a4db16f9e0a9e7ba8dec8da3820c75
George Shaikovski <g.shaikov@gmail.com>
2025-11-25 17:00:00 +0000

    ddpg_eval: log eval to wandb

--- rl_l171/algos/ddpg_eval.py
+++ rl_l171/algos/ddpg_eval.py
@@ -4,13 +4,14 @@ python3 -m rl_l171.algos.ddpg_eval
 
 from functools import partial
 from typing import Callable
+from dataclasses import dataclass
 
 import gymnasium as gym
 import numpy as np
 import torch
 import torch.nn as nn
-import tyro
-from rl_l171.algos.ddpg import Actor, QNetwork, make_env, Args
+
+from rl_l171.algos.ddpg import Actor, QNetwork, make_env, Args, VIDEO_ROOT
 
 
 def evaluate(
@@ -54,18 +55,38 @@ def evaluate(
                 episodic_returns.append(info["episode"]["r"])
         obs = next_obs
 
+    envs.close()
     return np.concatenate(episodic_returns)
 
 
+@dataclass
+class EvalArgs(Args):
+    seed: int = 42
+    wandb_project_name: str = "rl171_eval"
+    run_name: str = "Cubes-v0__ddpg__1__1764088346"
+
+
 if __name__ == "__main__":
-    args = tyro.cli(Args)
+    import wandb
+    import tyro
 
-    run_name = "Cubes-v0__ddpg__1__1764073507"
-    exp_name = "ddpg"
-    model_path = f"runs/{run_name}/{exp_name}.cleanrl_model"
-    run_name_eval = f"{run_name}-eval"
+    args = tyro.cli(EvalArgs)
 
-    evaluate(
+    model_path = f"runs/{args.run_name}/{args.exp_name}.cleanrl_model"
+
+    wandb_run = wandb.init(
+        project=args.wandb_project_name,
+        entity=args.wandb_entity,
+        config=vars(args),
+        name=args.run_name,
+        monitor_gym=True,
+        save_code=True,
+    )
+
+    run_name_eval = f"{args.run_name}-eval"
+    video_dir = VIDEO_ROOT / run_name_eval
+
+    episodic_returns = evaluate(
         model_path,
         partial(
             make_env,
@@ -74,7 +95,7 @@ if __name__ == "__main__":
             idx=0,
             capture_video=True,
             run_name=run_name_eval,
-            env_kwargs={"render_mode": "rgb_array"},
+            env_kwargs={"render_mode": "rgb_array", "max_nr_steps": 500},
             video_trigger=lambda _: True,
         ),
         eval_episodes=4,
@@ -82,3 +103,23 @@ if __name__ == "__main__":
         device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         exploration_noise=0,
     )
+
+    log = {}
+    log.update(
+        {
+            "eval/return_mean": episodic_returns.mean().item(),
+            "eval/return_std": episodic_returns.std().item(),
+        }
+    )
+    wandb_run.log(log)
+
+    for vid_file in video_dir.glob("*.mp4"):
+        wandb_run.log(
+            {
+                f"eval/video_{vid_file.stem}": wandb.Video(
+                    str(vid_file), caption=vid_file.stem, fps=30, format="mp4"
+                )
+            }
+        )
+
+    wandb_run.finish()
