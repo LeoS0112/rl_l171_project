f87c281c4417f9fb832f3123db420ebf6e1ab2e1
George Shaikovski <g.shaikov@gmail.com>
2025-11-25 16:32:13 +0000

    ddpg: scale rewards by 500

--- rl_l171/algos/ddpg.py
+++ rl_l171/algos/ddpg.py
@@ -266,6 +266,7 @@ if __name__ == "__main__":
 
         # TRY NOT TO MODIFY: execute the game and log data.
         next_obs, rewards, terminations, truncations, infos = envs.step(actions)
+        rewards *= 500
 
         # TRY NOT TO MODIFY: record rewards for plotting purposes
         if "final_info" in infos:
