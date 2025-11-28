775a2d396aacd2ceeac242cc2bfb9363d907497c
George Shaikovski <g.shaikov@gmail.com>
2025-11-28 12:56:42 +0000

    revert buffer to defaul code

--- rl_l171/algos/ddpg.py
+++ rl_l171/algos/ddpg.py
@@ -99,7 +99,9 @@ class Args:
     priority_inverse_critic
     priority_streaming
     """
-    buffer_strategy = "priority_streaming"
+    buffer_strategy: str = "priority_critic"
+    pb_beta: float = 0.0
+    pb_beta_increment_per_sampling: float = 0.0
 
 
 def make_env(
@@ -314,6 +316,8 @@ if __name__ == "__main__":
                 envs.single_action_space,
                 device,
                 handle_timeout_termination=False,
+                beta=args.pb_beta,
+                beta_increment_per_sampling=args.pb_beta_increment_per_sampling,
             )
         case "priority_streaming":
             rb = PriorityStreamingBuffer(
